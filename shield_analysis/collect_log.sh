682fe0fbba65646036ab374f9307a94d04d68a95
Zikang <zikangxiong@gmail.com>
2019-02-12 23:01:58 -0500

    necessity log

--- shield_analysis/collect_log.sh
+++ shield_analysis/collect_log.sh
@@ -3,15 +3,16 @@
 # @Last Modified by:   Zikang Xiong
 # @Last Modified time: 2019-02-10 19:33:27
 
-python 4-car-platoon/4-car-platoon.py >> 4-car-platoon/4-car-platoon.log
-python cartpole_change_l/cartpole_change_l.py >> cartpole_change_l/cartpole_change_l.log
-python cartpole_continous/cartpole_continous.py >> cartpole_continous/cartpole_continous.log
-python lanekeeping/lanekeeping.py >> lanekeeping/lanekeeping.log
-python oscillator10/oscillator10.py >> oscillator10/oscillator10.log
-python oscillator18/oscillator18.py >> oscillator18/oscillator18.log
-python pendulum_continous/pendulum_continous.py >> pendulum_continous/pendulum_continous.log
-python pendulum_continuous_change_l/pendulum_continuous_change_l.py >> pendulum_continuous_change_l/pendulum_continuous_change_l.log
-python pendulum_continuous_change_m/pendulum_continuous_change_m.py >> pendulum_continuous_change_m/pendulum_continuous_change_m.log
-python quadcopter/quadcopter.py >> quadcopter/quadcopter.log
-python selfdriving/selfdriving.py >> selfdriving/selfdriving.log
-python selfdriving_obs/selfdriving_obs.py >> selfdriving_obs/selfdriving_obs.log
+#python 4-car-platoon/4-car-platoon.py >> 4-car-platoon/4-car-platoon.log
+#python cartpole_change_l/cartpole_change_l.py >> cartpole_change_l/cartpole_change_l.log
+python cartpole_continuous/cartpole_continuous.py >> cartpole_continuous/cartpole_continuous.log
+#python lanekeeping/lanekeeping.py >> lanekeeping/lanekeeping.log
+# retrain osc
+#python oscillator10/oscillator10.py >> oscillator10/oscillator10.log
+#python oscillator18/oscillator18.py >> oscillator18/oscillator18.log
+python pendulum_continuous/pendulum_continuous.py >> pendulum_continuous/pendulum_continuous.log
+#python pendulum_continuous_change_l/pendulum_continuous_change_l.py >> pendulum_continuous_change_l/pendulum_continuous_change_l.log
+#python pendulum_continuous_change_m/pendulum_continuous_change_m.py >> pendulum_continuous_change_m/pendulum_continuous_change_m.log
+#python quadcopter/quadcopter.py >> quadcopter/quadcopter.log
+#python selfdriving/selfdriving.py >> selfdriving/selfdriving.log
+#python selfdriving_obs/selfdriving_obs.py >> selfdriving_obs/selfdriving_obs.log
